b03ebadbb52c2794973ff67a0628efcea729683c
Harry <harry@Harryui-noteubug.local>
2023-10-08 19:20:14 +0900

    [Chore] Completed layout BookViewController

--- ARTX-PoliceScience/ARTX-PoliceScience/ViewController/BookViewController.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/ViewController/BookViewController.swift
@@ -11,19 +11,87 @@ class BookViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setup()
+        layout()
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setup() {
+        
+        self.navigationController?.navigationBar.topItem?.title = "조현 경찰학이란?"
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        view.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(mainImageView)
+        mainStackView.addArrangedSubview(introLabelStack)
+        
+        introLabelStack.addArrangedSubview(introTitleLabel)
+        introLabelStack.addArrangedSubview(introSubLabel)
+//        introLabelStack.addArrangedSubview(emptyView)
+        
     }
-    */
-
+    
+    private func layout() {
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+        ])
+    }
+    
+    let mainStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        
+        return stackView
+    }()
+    
+    let mainImageView: UIImageView = {
+        let image = UIImage(named: "Thumbnail")
+        let imageView = UIImageView(image: image)
+        imageView.heightAnchor.constraint(equalToConstant: 308).isActive = true
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    let introLabelStack: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.backgroundColor = .white
+        stackView.spacing = 20
+        stackView.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        return stackView
+    }()
+    
+    let introTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = .black
+        label.text = "헤드라인"
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let introSubLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.textColor = .black
+        label.text = "상세설명"
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let emptyView: UIView = {
+        let view = UIView(frame: .zero)
+        return view
+    }()
+    
+    
 }
