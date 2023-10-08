2c385c5a7cbb9d7527492412f42f5188bc002430
Harry <harry@Harryui-noteubug.local>
2023-10-08 17:29:11 +0900

    [Feat] Create BookTableViewCell in the Views folder

---
+++ ARTX-PoliceScience/ARTX-PoliceScience/Views/BookTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookTableViewCell.swift
+//  ARTX-PoliceScience
+//
+//  Created by Harry on 2023/10/08.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    static let cellId = "BookTableViewCell"
+    
+}
