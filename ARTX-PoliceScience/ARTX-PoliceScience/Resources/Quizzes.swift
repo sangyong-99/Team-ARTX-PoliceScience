f4312e2ae5ff827e76fb13db54a76e91e460c050
Harry <harry@Harryui-noteubug.local>
2023-10-10 23:38:34 +0900

    [Feat] Completed QuizView Initial Setup

---
+++ ARTX-PoliceScience/ARTX-PoliceScience/Resources/Quizzes.swift
@@ -0,0 +1,18 @@
+//
+//  Quizzes.swift
+//  ARTX-PoliceScience
+//
+//  Created by Harry on 2023/10/10.
+//
+
+import UIKit
+
+enum Badge {
+    static let CompletionImage = UIImage(named: "IconCompletion")
+    static let NotCompletionImage = UIImage(named: "IconNotCompletion")
+}
+
+enum Bookmark {
+    static let play = UIImage(systemName: "bookmark", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32))
+    static let pause = UIImage(systemName: "bookmark.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32))
+}
--- ARTX-PoliceScience/ARTX-PoliceScience/ViewController/QuizViewController.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/ViewController/QuizViewController.swift
@@ -8,22 +8,110 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    
+    let chapterStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 4
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    let chapterNumberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = UIColor(named: "TextBlue")
+        label.text = "CHAPTER 01"
+        return label
+    }()
+    
+    let chpaterTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.textColor = UIColor(named: "PrimaryBlack")
+        label.text = "경찰 개념의 연혁"
+        return label
+    }()
+    
+    lazy var progressView: UIProgressView = {
+        let view = UIProgressView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.trackTintColor = .white
+        view.progressTintColor = UIColor(named: "BarPoint")
+        view.progress = 0.1
+        return view
+    }()
+    
+    let progressNumberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.textColor = .secondaryLabel
+        label.text = "0 / 30"
+        return label
+    }()
+    
+    let badgeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let quizNumberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.textColor = UIColor(named: "PrimaryBlack")
+        label.text = "Quiz 1"
+        return label
+    }()
+    
+    var bookMarkBautton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = UIColor(named: "TextBlue")
+        return button
+    }()
+    
+    let quizLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.textColor = UIColor(named: "PrimaryBlack")
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
+        label.text = "감찰관은 소속 경찰기관의 관할구역 안에서 활동하는 것을 원칙으로 한다."
+        return label
+    }()
+    
+    var correctBautton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.gray
+        return button
+    }()
+    
+    var wrongBautton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.gray
+        return button
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setup() {
+        
     }
-    */
-
+    
+    private func layout() {
+        
+    }
 }
