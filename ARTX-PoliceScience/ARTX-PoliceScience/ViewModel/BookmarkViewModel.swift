496476ca850106ae9a2cf85f5f635494115ff10a
HansolWorld <hs9269@gmail.com>
2023-10-12 21:20:58 +0900

    [Chore] Add dummy BookmarkViewModel

---
+++ ARTX-PoliceScience/ARTX-PoliceScience/ViewModel/BookmarkViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BookmarkViewModel.swift
+//  ARTX-PoliceScience
+//
+//  Created by apple on 10/12/23.
+//
+
+import Foundation
+
+class BookmarkViewModel {
+
+    func getChapterNumber() -> Int {
+        return 0
+    }
+    
+    func getChapterTitle() -> String {
+        return ""
+    }
+    
+    func getBookmarkQuizeCount() -> Int {
+        return 0
+    }
+}
