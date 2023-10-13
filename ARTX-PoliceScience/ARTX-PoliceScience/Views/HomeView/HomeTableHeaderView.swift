5cff6d6a5ef35476bb0816326f9ef9558c2875bd
Jae Ho Yoon (Marcus) <mr.jaeho@gmail.com>
2023-10-14 02:09:26 +0900

    [Feat] HomeTableHeaderView UI

--- ARTX-PoliceScience/ARTX-PoliceScience/Views/HomeView/HomeTableHeaderView.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/Views/HomeView/HomeTableHeaderView.swift
@@ -19,6 +19,31 @@ class HomeTableHeaderView: UIView {
         return imageView
     }()
     
+    private let homeTitle: UILabel = {
+        let label = UILabel()
+        label.text = "조현 경찰학\nOX 문제집"
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        label.numberOfLines = 2
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        // 글씨체 Gmarket Sans로 변경 필요
+        let attributedString = NSMutableAttributedString(string: label.text!)
+        attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 34, weight: .medium), range: (label.text! as NSString).range(of:"OX 문제집"))
+        label.attributedText = attributedString
+        
+        return label
+    }()
+    
+    private let settingViewButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "gearshape.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 22, weight: .bold)), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(settingViewButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     private let progressView: UIView = {
         let progress = UIView()
         progress.backgroundColor = .white
@@ -49,9 +74,10 @@ class HomeTableHeaderView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(bgImage)
+        self.addSubview(settingViewButton)
+        self.addSubview(homeTitle)
         self.addSubview(progressView)
         self.addSubview(progressViewTitle)
-        
         layout()
     }
     
@@ -68,6 +94,13 @@ extension HomeTableHeaderView {
             bgImage.heightAnchor.constraint(equalTo: self.heightAnchor),
             bgImage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -185),
             
+            settingViewButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 58),
+            settingViewButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
+            
+            homeTitle.bottomAnchor.constraint(equalTo: progressView.topAnchor, constant: -24),
+            homeTitle.leadingAnchor.constraint(equalTo: progressView.leadingAnchor),
+            homeTitle.trailingAnchor.constraint(equalTo: progressView.trailingAnchor),
+            
             progressView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
             progressView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
             progressView.heightAnchor.constraint(equalToConstant: 142),
@@ -76,5 +109,11 @@ extension HomeTableHeaderView {
             progressViewTitle.leadingAnchor.constraint(equalTo: progressView.leadingAnchor, constant: 16),
             progressViewTitle.topAnchor.constraint(equalTo: progressView.topAnchor, constant: 16)
         ])
+    }
+}
+
+extension HomeTableHeaderView {
+    @objc func settingViewButtonTapped(sender: UIButton) {
+        print("tapped")
     }
 }
