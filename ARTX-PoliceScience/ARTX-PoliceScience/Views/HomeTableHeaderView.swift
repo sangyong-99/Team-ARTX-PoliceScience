7c5a4e35d6024c662baff2a733d8ad12e58c96c5
Jae Ho Yoon (Marcus) <mr.jaeho@gmail.com>
2023-10-14 00:21:16 +0900

    [Feat] Add ProgressView in HomeTableHeaderView

--- ARTX-PoliceScience/ARTX-PoliceScience/Views/HomeTableHeaderView.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/Views/HomeTableHeaderView.swift
@@ -19,9 +19,38 @@ class HomeTableHeaderView: UIView {
         return imageView
     }()
     
+    private let progressView: UIView = {
+        let progress = UIView()
+        progress.backgroundColor = .white
+        progress.layer.cornerRadius = 14
+        progress.translatesAutoresizingMaskIntoConstraints = false
+        
+        progress.layer.shadowColor = UIColor(red: 0.102, green: 0.176, blue: 0.561, alpha: 0.1).cgColor
+        progress.layer.shadowOpacity = 1
+        progress.layer.shadowRadius = 8
+        progress.layer.shadowOffset = CGSize(width: 4, height: 4)
+        
+        return progress
+    }()
+    
+    private let progressViewTitle: UILabel = {
+        let label = UILabel()
+        let attributedString = NSMutableAttributedString(string: "")
+        let imageAttachment = NSTextAttachment()
+        imageAttachment.image = UIImage(systemName: "chart.bar.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 12))
+        attributedString.append(NSAttributedString(attachment: imageAttachment))
+        attributedString.append(NSAttributedString(string: " 전체 학습 진행도"))
+        label.attributedText = attributedString
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(bgImage)
+        self.addSubview(progressView)
+        self.addSubview(progressViewTitle)
         
         layout()
     }
@@ -37,7 +66,15 @@ extension HomeTableHeaderView {
             bgImage.widthAnchor.constraint(equalTo: self.widthAnchor),
             bgImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             bgImage.heightAnchor.constraint(equalTo: self.heightAnchor),
-            bgImage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -185)
+            bgImage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -185),
+            
+            progressView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
+            progressView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
+            progressView.heightAnchor.constraint(equalToConstant: 142),
+            progressView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -30),
+            
+            progressViewTitle.leadingAnchor.constraint(equalTo: progressView.leadingAnchor, constant: 16),
+            progressViewTitle.topAnchor.constraint(equalTo: progressView.topAnchor, constant: 16)
         ])
     }
 }
