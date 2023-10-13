50556e4570bb07ab3f8aa5a232702227667ee42f
HansolWorld <hs9269@gmail.com>
2023-10-13 22:53:59 +0900

    [Feat] Fix dummy count

--- ARTX-PoliceScience/ARTX-PoliceScience/ViewModel/BookmarkViewModel.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/ViewModel/BookmarkViewModel.swift
@@ -9,27 +9,31 @@ import Foundation
 
 class BookmarkViewModel {
 
+    func getPartNumber() -> Int {
+        return 1
+    }
+    
+    func getPartTitle() -> String {
+        return "경찰학 기초"
+    }
+    
     func getChapterNumber() -> Int {
         return 3
     }
     
     func getChapterTitle() -> String {
-        return ""
-    }
-    
-    func getBookmarkQuizeCount() -> Int {
-        return 0
-    }
-    
-    func getChapterCount() -> Int {
-        return 3
+        return "경찰 연혁"
     }
     
     func getPartCount() -> Int {
         return 3
     }
     
-    func getQuizeCount() -> Int {
+    func getChapterCount() -> Int {
         return 3
+    }
+    
+    func getBookmarkQuizeCount() -> Int {
+        return 0
     }
 }
