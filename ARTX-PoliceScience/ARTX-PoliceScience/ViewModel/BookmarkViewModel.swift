8a85df074ca61c17c123e96c7b224b6fed4e9cfa
HansolWorld <hs9269@gmail.com>
2023-10-14 15:44:29 +0900

    [Feat] Apply viewmodel

--- ARTX-PoliceScience/ARTX-PoliceScience/ViewModel/BookmarkViewModel.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/ViewModel/BookmarkViewModel.swift
@@ -10,12 +10,31 @@ import Foundation
 class BookmarkViewModel {
     private let Quizs: [Quiz] = [
             Quiz(part: 1, partName: "경찰학", chapters: [
-                Chapter(chapter: 1, chapterName: "경찰 연혁", questions: [
+                Chapter(chapter: 1, chapterName: "경찰이란", questions: [
                     Question(question: "문제 1", answer: "O", explanation: "해설 1"),
                     Question(question: "문제 2", answer: "X", explanation: "해설 2"),
                     Question(question: "문제 3", answer: "O", explanation: "해설 3")
+                ]),
+                Chapter(chapter: 2, chapterName: "경찰 연혁", questions: [
+                    Question(question: "문제 1", answer: "O", explanation: "해설 1")
                 ])
-            ])
+            ]),
+            Quiz(part: 2, partName: "경찰행정", chapters: [
+                Chapter(chapter: 1, chapterName: "경찰행정법", questions: [
+                    Question(question: "문제 1", answer: "O", explanation: "해설 1"),
+                    Question(question: "문제 2", answer: "X", explanation: "해설 2"),
+                    Question(question: "문제 3", answer: "O", explanation: "해설 3"),
+                    Question(question: "문제 4", answer: "O", explanation: "해설 4")
+                ])
+            ]),
+            Quiz(part: 3, partName: "경찰메롱", chapters: []),
+            Quiz(part: 4, partName: "경찰법률", chapters: [
+                 Chapter(chapter: 1, chapterName: "경찰의 미래", questions: [
+                     Question(question: "문제 1", answer: "O", explanation: "해설 1"),
+                     Question(question: "문제 2", answer: "X", explanation: "해설 2"),
+                     Question(question: "문제 3", answer: "O", explanation: "해설 3")
+                 ])
+             ]),
         ]
     private var currentPart: Int = 0
     private var currentChapter: Int = 0
