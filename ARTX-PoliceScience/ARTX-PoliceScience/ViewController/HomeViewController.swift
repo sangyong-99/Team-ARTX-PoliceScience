d365adf2db457f1689c4618f8c7cc38fd1936093
Jae Ho Yoon (Marcus) <mr.jaeho@gmail.com>
2023-10-18 13:16:35 +0900

    [Feat] ToastMessage Delegate

--- ARTX-PoliceScience/ARTX-PoliceScience/ViewController/HomeViewController.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/ViewController/HomeViewController.swift
@@ -485,6 +485,7 @@ extension HomeViewController {
         } else {
             switch currentQuizNumber {
             case 0:
+                quizViewController.delegate = self
                 navigationController?.pushViewController(quizViewController, animated: true)
                 navigationController?.isNavigationBarHidden = false
             case totalQuizNumber:
@@ -583,4 +584,26 @@ extension HomeViewController {
     
 }
 
-
+extension HomeViewController: QuizViewConrollerDelegate {
+    private func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
+    func chapterFinished() {
+        showToast(message: "학습을 완료하였습니다!")
+    }
+}
