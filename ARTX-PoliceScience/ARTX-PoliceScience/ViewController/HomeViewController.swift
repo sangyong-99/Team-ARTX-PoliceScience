f756d4f041347abfb0929c8d56df243e4d00d613
Jae Ho Yoon (Marcus) <mr.jaeho@gmail.com>
2023-10-20 22:30:31 +0900

    Toast Message Fix

--- ARTX-PoliceScience/ARTX-PoliceScience/ViewController/HomeViewController.swift
+++ ARTX-PoliceScience/ARTX-PoliceScience/ViewController/HomeViewController.swift
@@ -590,15 +590,15 @@ extension HomeViewController {
 
 extension HomeViewController: QuizViewConrollerDelegate {
     private func showToast(message : String, font: UIFont = UIFont.callOut) {
-        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 175, height: 32))
         toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
         toastLabel.textColor = UIColor.white
         toastLabel.font = font
-        toastLabel.textAlignment = .center;
+        toastLabel.textAlignment = .center
         toastLabel.text = message
         toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds  =  true
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
         self.view.addSubview(toastLabel)
         UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
             toastLabel.alpha = 0.0
